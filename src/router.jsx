3105362c35e226ec30573a75103559878ddc0e1b
johhny47 <mdjohnny063@gmail.com>
2024-12-21 20:09:46 +0600

    add NavBar

--- src/router.jsx
+++ src/router.jsx
@@ -4,12 +4,24 @@ import {
   } from "react-router-dom";
 import MainLayout from "./Layout/MainLayout";
 import Home from "./Page/Home";
+import Login from "./Page/Login";
+import Registration from "./Page/Registration";
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
       children: [
+        {
+            path: "/login",
+            element:<Login></Login>
+
+        },
+        {
+            path: "/registration",
+            element:<Registration></Registration>
+
+        },
         {
             path: "/",
             element: <Home></Home>
