f8fb60a48805c32d6bfd1afcb91eb3eb6175b8c3
johhny47 <mdjohnny063@gmail.com>
2024-12-24 19:25:52 +0600

    add banner

---
+++ src/Component/Banner.jsx
@@ -0,0 +1,42 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation, Pagination, Scrollbar } from "swiper/modules";
+
+import 'swiper/css';
+import 'swiper/swiper-bundle.css'
+import banner1 from '../assets/sample-1.jpg'
+import banner2 from '../assets/sample-2.jpg'
+import banner3 from '../assets/sample-3.jpg'
+
+const Banner = () => {
+    return (
+        <div className='h-[450px] md:h-[600px] lg:h-[600px]'>
+          {
+             <Swiper
+             modules={[Navigation, Pagination, Scrollbar, A11y]}
+             spaceBetween={50}
+             slidesPerView={1}
+             navigation
+             pagination={{ clickable: true }}
+             scrollbar={{ draggable: true }}
+           
+            
+           >
+             <SwiperSlide>
+             <img src={banner1}/>
+             </SwiperSlide>
+             <SwiperSlide>
+             <img src={banner2}/>
+             </SwiperSlide>
+             <SwiperSlide>
+             <img src={banner3}/>
+             </SwiperSlide>
+           
+           </Swiper>
+          }
+  
+
+        </div>
+    );
+};
+
+export default Banner;
